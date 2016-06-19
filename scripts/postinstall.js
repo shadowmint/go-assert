e490a4b3900c8894feb72afd33f742ff10956fff
Douglas Linder <linderd@iinet.net.au>
2016-06-19 17:03:52 +0800

    use normal vendoring

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -7,7 +7,7 @@ var package = "n-core";
 
 // Paths
 var src = path.join(__dirname, '..', 'src');
-var dir = path.join(__dirname, '..', '..', '..', 'vendor', 'src');
+var dir = path.join(__dirname, '..', '..', '..', 'src', 'vendor');
 
 // Create folder if missing
 mkdirp(dir, function (err) {
