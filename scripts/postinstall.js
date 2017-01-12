b76785e0602c1d820e81c25d581fdf130c654967
Douglas Linder <douglasl@signiq.com>
2017-01-12 09:56:23 +0800

    install script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,21 +3,25 @@ var path = require('path');
 var ncp = require('ncp');
 
 // Paths
-var src = path.join(__dirname, '..', 'src');
-var dir = path.join(__dirname, '..', '..', '..', 'src', 'vendor');
+var ns = 'ntoolkit';
+var src = path.join(__dirname, '..', 'src', ns);
+var dir = path.join(__dirname, '..', '..', '..', 'src', 'vendor', ns);
+var ismodule = __dirname.split(path.sep).filter(function(i) { return i == 'node_modules'; });
 
 // Create folder if missing
-mkdirp(dir, function (err) {
-  if (err) {
-    console.error(err)
-    process.exit(1);
-  }
-
-  // Copy files
-  ncp(src, dir, function (err) {
+if (ismodule) {
+  mkdirp(dir, function (err) {
     if (err) {
-      console.error(err);
+      console.error(err)
       process.exit(1);
     }
+
+    // Copy files
+    ncp(src, dir, function (err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
   });
-});
+}
